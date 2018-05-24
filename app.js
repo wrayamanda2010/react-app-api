29a4e4c9f0d55b519c4a2c9198a50201c4ee1a0e
Amanda Wray <amandawray2010@gmail.com>
2018-05-24 11:31:35 -0600

    No more CORS issues

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE, PUT')
+  res.header('Referrer-Policy', 'no-referrer')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  else {
+    next()
+  }
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
